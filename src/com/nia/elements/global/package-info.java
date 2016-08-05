e74ec2693619d1d41898070111d82fe92b537f70
Thomas Woltjer <twtduck.ii@gmail.com>
2016-08-05 15:47:29 -0700

    Removed files from master that no longer belong here. Particularly source files.

--- src/com/nia/elements/global/package-info.java
+++
@@ -1,6 +0,0 @@
-
-/**
- * This package contains classes that are used globally throughout the program.
- * @author ducky
- */
-package com.nia.elements.global;
--- src/com/nia/elements/package-info.java
+++
@@ -1,6 +0,0 @@
-
-/**
- * This class is to define classes specific to the NIA program.
- * @author Thomas Woltjer
- */
-package com.nia.elements;
