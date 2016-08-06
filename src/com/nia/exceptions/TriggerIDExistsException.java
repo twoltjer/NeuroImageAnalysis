17b0dd1f2357e4dfa89d3afd8143801f2f2c57f2
Thomas Woltjer <twtduck.ii@gmail.com>
2016-08-05 22:35:50 -0700

    Cleaned branch and added patcher v1.0 compiled from patcher-source branch

--- src/com/nia/exceptions/TriggerIDExistsException.java
+++
@@ -1,9 +0,0 @@
-package com.nia.exceptions;
-
-public class TriggerIDExistsException extends Exception {
-	/**
-	 * Serial version unique identifier. 
-	 */
-	private static final long serialVersionUID = 7795105993792191009L;
-
-}
